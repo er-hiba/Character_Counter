8e6c34f38f26f656813e88cbac8eba5068566094
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-10-16 22:59:12 +0100

    :)

---
+++ Character_Counter.py
@@ -0,0 +1,12 @@
+# Initialize a counter to zero
+counter = 0
+
+# Ask the user to input a string
+string = input("Enter a string: ")
+
+# Iterate through each character in the string
+for character in string:
+    counter += 1
+
+# Display the number of characters in the string
+print("The number of characters in the string is:", counter)
